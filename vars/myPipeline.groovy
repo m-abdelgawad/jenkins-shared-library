5f75fa923242e9b39c179364c42b432a729feb3c
Mohamed AbdelGawad Ibrahim <68827158+m-abdelgawad@users.noreply.github.com>
2024-01-22 20:13:00 +0200

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -6,7 +6,7 @@ def call(Map pipelineParams) {
             stage('Preparation') {
                 steps {
                     script {
-                        echo 'Hello world!'
+                        echo pipelineParams['name']
                     }
                 }
             }
