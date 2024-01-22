5cb833d518110d46fcac71c828850bd0d93c056e
Mohamed AbdelGawad Ibrahim <68827158+m-abdelgawad@users.noreply.github.com>
2024-01-22 23:28:48 +0200

    Update djangoPipeline.groovy

--- vars/djangoPipeline.groovy
+++ vars/djangoPipeline.groovy
@@ -15,19 +15,7 @@ def call(Map varsMap) {
             stage('Preparation') {
                 steps {
                     script {
-                        sh '''
-                            apt update
-                            apt install -y ca-certificates curl gnupg
-                            install -m 0755 -d /etc/apt/keyrings
-                            curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-                            chmod a+r /etc/apt/keyrings/docker.gpg
-                            echo \
-                              "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-                              "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-                              tee /etc/apt/sources.list.d/docker.list > /dev/null
-                            apt update
-                            apt install -y docker-ce-cli docker-compose-plugin
-                        '''
+                        
                         // Clean workspace
                         wsTools.clean()
 
@@ -45,6 +33,21 @@ def call(Map varsMap) {
                         // Set new image name
                         newImage = varsMap['dockerhubRepo'] + ":" + commitId
                         echo "New image tag will be \'${newImage}\'"
+
+                        sh '''
+                            apt update
+                            apt install -y ca-certificates curl gnupg
+                            install -m 0755 -d /etc/apt/keyrings
+                            curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                            chmod a+r /etc/apt/keyrings/docker.gpg
+                            echo \
+                              "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+                              "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+                              tee /etc/apt/sources.list.d/docker.list > /dev/null
+                            apt update
+                            apt install -y docker-ce-cli docker-compose-plugin
+                            docker build . -t test
+                        '''
                         
                     } // end script
                 } // end steps
