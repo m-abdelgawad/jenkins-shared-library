08599bd83284c519704ddabb54394e30d8651afd
Mohamed AbdelGawad Ibrahim <68827158+m-abdelgawad@users.noreply.github.com>
2024-01-22 23:05:18 +0200

    Update pythonTools.groovy

--- src/pythonTools.groovy
+++ src/pythonTools.groovy
@@ -1,10 +1,10 @@
 def installReq(reqPath) {
   script {
-    sh '''
+    sh """
       . venv/bin/activate
       python --version
       echo "Installing requirements from: ${reqPath}"
       pip install -r ${reqPath}
-    '''
+    """
   }
 }
