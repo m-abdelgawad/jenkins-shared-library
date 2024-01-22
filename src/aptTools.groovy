e3ef80f699e29b6af20705fdaab0ae314eb5d0b4
Mohamed AbdelGawad Ibrahim <68827158+m-abdelgawad@users.noreply.github.com>
2024-01-22 22:48:26 +0200

    Update aptTools.groovy

--- src/aptTools.groovy
+++ src/aptTools.groovy
@@ -1,8 +1,19 @@
-def updateAptPackages() {
+def updatePackages() {
   script {
-      sh '''
-          apt update
-          apt -y upgrade
-      '''
+    sh '''
+      apt update
+      apt -y upgrade
+    '''
+  }
+}
+
+def installPython() {
+  script {
+    sh '''
+      apt install -y python3 python3-pip build-essential libssl-dev libffi-dev python3-dev python3-venv
+      python3 -m venv venv
+      . venv/bin/activate
+      python --version
+    '''
   }
 }
