e5ecaab47cc2de4356e37c0defdd7d39eb9fade2
Mohamed AbdelGawad Ibrahim <68827158+m-abdelgawad@users.noreply.github.com>
2024-01-22 20:59:40 +0200

    Update djangoPipeline.groovy

--- vars/djangoPipeline.groovy
+++ vars/djangoPipeline.groovy
@@ -19,6 +19,10 @@ def call(Map pipelineParams) {
                         // Get commit ID
                         commitId = preparation.getCommitId()
                         echo "Commit ID is ${commitId}"
+
+                        // Get commit meassage
+                        commitMsg = preparation.getCommitMsg()
+                        echo "Commit message is ${commitMsg}"
                     }
                 }
             }
