17a311f70e7cb0ef1553cbc04e42aebce4afccbf
Mohamed AbdelGawad Ibrahim <68827158+m-abdelgawad@users.noreply.github.com>
2024-01-22 20:59:15 +0200

    Update preparation.groovy

--- src/preparation.groovy
+++ src/preparation.groovy
@@ -9,3 +9,7 @@ def checkoutRepo() {
 def getCommitId() {
   return sh(returnStdout: true, script: "git rev-parse --short HEAD").trim()
 }
+
+def getCommitMsg() {
+  return sh(returnStdout: true, script: 'git log -1 --pretty=format:"%s"').trim()
+}
