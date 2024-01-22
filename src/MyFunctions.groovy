e624fb2858cdf8e35e00c04068af218e65d6daf7
Mohamed AbdelGawad Ibrahim <68827158+m-abdelgawad@users.noreply.github.com>
2024-01-22 20:18:36 +0200

    Update MyFunctions.groovy

--- src/MyFunctions.groovy
+++ src/MyFunctions.groovy
@@ -1,7 +1,5 @@
 // src/MyFunctions.groovy
 
-class MyFunctions {
-    static def preparation(Map params) {
-        println "Hello from preparation! Accessing pipelineParams: ${params}"
-    }
+def preparation(Map params) {
+    println "Hello from preparation! Accessing pipelineParams: ${params}"
 }
